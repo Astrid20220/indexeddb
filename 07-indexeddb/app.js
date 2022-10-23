2e27b3f36696f7637b41294a03b3d8be04d1d91d
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-22 20:54:31 -0500

    Manejo de errores e inserción de registros

--- 07-indexeddb/app.js
+++ 07-indexeddb/app.js
@@ -14,4 +14,46 @@ request.onupgradeneeded = event =>{
     });
 };
 
+//Manejo de errores
+request.onerror = event => {
+    console.log('DB error', event.target.error);
+};
+
+//insertar datos
+request.onsuccess = event =>{
+
+    let db = event.target.result;
+    let heroesData = [
+        { id: '1111', heroe:'Spidermar', mensaje: 'Aqui su amigo y buen vecino el hombre araña'},
+        { id: '2222', heroe:'Ironman', mensaje: 'Aqui en mi nuevo Mark 50'}
+        
+
+    ];
+
+    let heroesTransaction = db.transaction('heroes', 'readwrite');
+    heroesTransaction.onerror = event =>{
+        console.log('Error guardado', event.target.error);
+
+    };
+
+    //Informa sobre el exito de la transaccion
+    heroesTransaction.oncomplete = event =>{
+        console.log('Transaccion hecha', event);
+
+    };
+
+    let heroesStore = heroesTransaction.objectStore('heroes');
+
+    for(let heroe of heroesData){
+        heroesStore.add(heroe);
+
+    }
+
+    heroesStore.onsuccess = event => {
+        console.log('Nuevo item agregado a la base de datos');
+
+    };
+
+};
+
 

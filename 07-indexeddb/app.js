a27fa552eeb27c99c7f5928dd26c3db921845e4d
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-22 20:25:51 -0500

    Inicios en indexedDB

--- 07-indexeddb/app.js
+++ 07-indexeddb/app.js
@@ -1,6 +1,17 @@
 
 // indexedDB: Reforzamiento
 
+let request = window.indexedDB.open('mi-database',2);
 
+//Se actualiza cuando e crea o se sube de version de la DB
+request.onupgradeneeded = event =>{
+    console.log('Actualizacion de BD');
+
+    let db = event.target.result;
+
+    db.createObjectSt('heroes', {
+        keyPath: 'id'
+    });
+};
 
 
